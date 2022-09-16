f0669d78e156df7333c6bb0c40773c4a9eb5ddcb
Diana Vu <aznalive@hotmail.com>
2022-09-16 10:35:24 -0400

    added if functions

--- Develop/script.js
+++ Develop/script.js
@@ -10,23 +10,23 @@ function writePassword () {
 }
 
 /* this will run once the generate button is pressed, prompting the password length question */
-function generatePassword () {
+function generatePassword (number) {
   let pass = true;
   while (pass) {
     let passLength = prompt(
       'To start, please choose a password length between 8 to 128 characters'
-    ) 
-    // 
+    )
+    
     if (passLength === null){
       pass = false;
-    } 
+    }
     else if (passLength < 8 || passLength > 128) {
     }
     // checks if passLength is equal to not a number
     //isNaN is a function that will check if the value is a number or not, if a number it will return false, if not a number it will return true
     else if (isNaN(passLength)) {
       alert('This is an invalid number')
-    } 
+    }
     else {
       pass = false;
     }
@@ -38,13 +38,14 @@ function generatePassword () {
   let containNumber = confirm('Do you want to add numbers to your password?');
   let containSpecial = confirm('Do you want to add special characters to your password?');
 
-  upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  specialCharacters = ["!", "@", "%", "$", "*"];
-  
-
 }
+
+upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+specialCharacters = ['!', '@', '%', '$', '*']
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword)
-
