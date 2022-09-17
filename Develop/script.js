b50bd480504135df4a5303f8868e954aa42edf8e
Diana Vu <aznalive@hotmail.com>
2022-09-16 20:41:49 -0400

    added for loops for number array and special character array

--- Develop/script.js
+++ Develop/script.js
@@ -35,17 +35,47 @@ function writePassword (number) {
     }
   }
 
+  let upperC
+  let lowerC
+  let numbLi
+  let symb
   if (passLength !== null) {
-    let upperC = confirm(
-      'Do you want to add uppercase letters to your password?'
-    )
-    let lowerC = confirm(
-      'Do you want to add lowercase letters to your password?'
-    )
-    let numbLi = confirm('Do you want to add numbers to your password?')
-    let symb = confirm('Do you want to add special characters to your password?')
+    upperC = confirm('Do you want to add uppercase letters to your password?')
+    lowerC = confirm('Do you want to add lowercase letters to your password?')
+    numbLi = confirm('Do you want to add numbers to your password?')
+    symb = confirm('Do you want to add special characters to your password?')
+  }
+
+  let pool = ''
+  if (lowerC === true) {
+    pool += lowerCaseLetters
+  }
+  if (upperC === true) {
+    pool += upperCaseLetters
+  }
+  if (numbLi === true) {
+    for (let i = 0; i < numberList.length; i++) {
+      pool += numberList[i]
+    }
+  }
+  if (symb === true) {
+    for (let i = 0; i < specialCharacters.length; i++) {
+      pool += specialCharacters[i]
+    }
+  }
+
+  let pwd = ''
+  if (upperC === true) {
+    for (let i = 0; i < passLength; i++) {
+      /* once math.random function generates a random number within the uppercaseletter index it is then stored in the variable randomUpperC;*/
+      let randomIndexGen = Math.floor(Math.random() * pool.length)
+      /* since we have our number value stored in randomUpperC we can now create a variable that will give us the letter of that index by using upperCaseLetter variable and having [randomUpperC]
+      beside it since it is considered an index */
+      let randomChar = pool[randomIndexGen];
+      /* use the += when you're adding a value into a string */
+      pwd += randomChar
+    }
   }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword)
